7c04834061d133fab72ec510140c39ab327e5a02
Luciano <lushobarlett@gmail.com>
2022-01-05 12:16:22 -0300

    turn declaration file into a module

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,5 @@
+import { DependencyList, Dispatch, SetStateAction } from 'react';
+
 declare function sleep(delay: number): Promise<void>;
 
 declare type Process<T> = () => Promise<T>;
@@ -11,16 +13,16 @@ declare interface AsyncResult<T, E> {
 
 declare function useAsync<T, E = unknown>(
   process: Process<T>,
-  dependecyList: React.DependencyList
+  dependecyList: DependencyList
 ): AsyncResult<T, E>;
 
 declare function useAsyncState<T, R, TE = unknown, RE = unknown>(
   initial: T,
   refresh: Process<T>,
-  refreshDependecnyList: React.DependencyList,
+  refreshDependecnyList: DependencyList,
   sync: Process<R>,
-  syncDependencyList: React.DependencyList
-): [T, React.Dispatch<React.SetStateAction<T>>, AsyncResult<T, TE>, AsyncResult<R, RE>];
+  syncDependencyList: DependencyList
+): [T, Dispatch<SetStateAction<T>>, AsyncResult<T, TE>, AsyncResult<R, RE>];
 
 declare function useConst<T>(state: T): T;
 
@@ -30,12 +32,24 @@ declare function useToggle(
 
 declare function useLens<T extends Record<number | string, unknown>>(
   state: T,
-  setState: React.Dispatch<React.SetStateAction<T>>,
+  setState: Dispatch<SetStateAction<T>>,
   key: number | string
-): [T[typeof key], React.Dispatch<T[typeof key]>];
+): [T[typeof key], Dispatch<T[typeof key]>];
 
 declare function useLensPath<T extends Record<number | string, unknown>, L = unknown>(
   state: T,
-  setState: React.Dispatch<React.SetStateAction<T>>,
+  setState: Dispatch<SetStateAction<T>>,
   key: Array<number | string>
-): [L, React.Dispatch<L>];
+): [L, Dispatch<L>];
+
+export {
+  sleep,
+  type Process,
+  type AsyncResult,
+  useAsync,
+  useAsyncState,
+  useConst,
+  useToggle,
+  useLens,
+  useLensPath
+};
